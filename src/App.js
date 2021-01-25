49ac554ea7e9fdfb247aff47d68e060bc370f06c
Adonys <adonyssantoscemas@gmail.com>
2021-01-25 10:45:09 -0400

    Components folder added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,6 @@
 import React, { Component } from "react";
+import TodoList from "./components/TodoList.js";
 import "./App.css";
-
-class TodoList extends Component {
-  render() {
-    const { todos, toggleTodo, deleteTodo } = this.props;
-    return todos.map((todo, index) => {
-      return (
-        <li key={todo.title}>
-          <span className={`task ${todo.completed ? "completed" : ""}`}>
-            {todo.title}
-          </span>
-          <span
-            role="img"
-            aria-label="emoji"
-            className="icon-task"
-            onClick={toggleTodo}
-            data-index={index}
-            title="Mark as complete."
-          >
-            ✅
-          </span>
-          <span
-            role="img"
-            aria-label="emoji"
-            className="icon-task"
-            onClick={deleteTodo}
-            data-index={index}
-            title="Remove task."
-          >
-            ⛔
-          </span>
-        </li>
-      );
-    });
-  }
-}
 
 class App extends Component {
   state = {
